2a8e0b23149a96acf74b8fab84cbd8bc218aea39
ganjioppa <Timkim@Tims-MacBook.local>
2018-12-06 22:48:48 -0800

    updating

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Gal Gadot',
+		photo: 'https://shawetcanada.files.wordpress.com/2018/08/gettyimages-927261380.jpg?quality=80&strip=all&w=605&zoom=2',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Melissa McCarthy',
+		photo: 'https://shawetcanada.files.wordpress.com/2018/08/gettyimages-961801194.jpg?quality=80&strip=all&w=605&zoom=2',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Cate Blanchett',
+		photo: 'https://shawetcanada.files.wordpress.com/2018/08/gettyimages-969444482.jpg?quality=80&strip=all&w=605&zoom=2',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Julia Roberts',
+		photo: 'https://shawetcanada.files.wordpress.com/2018/08/gettyimages-531201744.jpg?quality=80&strip=all&w=605&zoom=2',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Mila Kunis',
+		photo: 'https://shawetcanada.files.wordpress.com/2018/08/gettyimages-951862132.jpg?quality=80&strip=all&w=605&zoom=2',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Reese Witherspoon',
+		photo: 'https://shawetcanada.files.wordpress.com/2018/08/gettyimages-947131244.jpg?quality=80&strip=all&w=605&zoom=2',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jennifer Lawrence',
+		photo: 'https://shawetcanada.files.wordpress.com/2018/08/gettyimages-921222214.jpg?quality=80&strip=all&w=605&zoom=2',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Jennifer Aniston',
+		photo: 'https://shawetcanada.files.wordpress.com/2018/08/gettyimages-966544606.jpg?quality=80&strip=all&w=605&zoom=2',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Angelina Jolie',
+		photo: 'https://shawetcanada.files.wordpress.com/2018/08/gettyimages-919975696.jpg?quality=80&strip=all&w=605&zoom=2',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Scarlett Johansson',
+		photo: 'https://shawetcanada.files.wordpress.com/2018/08/gettyimages-694964822.jpg?quality=80&strip=all&w=605&zoom=2',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
